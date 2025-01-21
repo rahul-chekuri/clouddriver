6b5722de86b39ed7baed2f6702897db746d52bc5
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2025-01-21 12:17:47 +0530

    refactor(Elasticsearch): Pinged Elasticsearch dependency.
    As part of springboot3 upgrade, org.elasticsearch:elasticsearch has been removed so pinged the dependency version explicitly.
    
    Error:
    ```
    Execution failed for task ':clouddriver-elasticsearch:compileGroovy'.
    > Could not resolve all files for configuration ':clouddriver-elasticsearch:compileClasspath'.
       > Could not find org.elasticsearch:elasticsearch:.
         Required by:
             project :clouddriver-elasticsearch
    
     clouddriver/clouddriver-web/src/main/groovy/com/netflix/spinnaker/clouddriver/Main.groovy: 27: unable to resolve class org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticSearchRestHealthContributorAutoConfiguration
      @ line 27, column 1.
        import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticSearchRestHealthContributorAutoConfiguration
        ^
    ```
    Links:
    https://docs.spring.io/spring-boot/docs/2.7.x/reference/html/dependency-versions.html
    https://docs.spring.io/spring-boot/docs/3.0.x/reference/html/dependency-versions.html

--- clouddriver-web/src/main/groovy/com/netflix/spinnaker/clouddriver/Main.groovy
+++ clouddriver-web/src/main/groovy/com/netflix/spinnaker/clouddriver/Main.groovy
@@ -24,20 +24,16 @@ import com.netflix.spinnaker.kork.artifacts.artifactstore.ArtifactStoreConfigura
 import com.netflix.spinnaker.kork.artifacts.model.Artifact
 import com.netflix.spinnaker.kork.boot.DefaultPropertiesBuilder
 import com.netflix.spinnaker.kork.configserver.ConfigServerBootstrap
-import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticSearchRestHealthContributorAutoConfiguration
+import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticsearchRestHealthContributorAutoConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration
 import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.*
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.scheduling.annotation.EnableScheduling
 
@@ -59,7 +55,7 @@ import java.security.Security
   GsonAutoConfiguration,
   DataSourceAutoConfiguration,
   ElasticsearchDataAutoConfiguration,
-  ElasticSearchRestHealthContributorAutoConfiguration
+  ElasticsearchRestHealthContributorAutoConfiguration
 ])
 @EnableScheduling
 class Main extends SpringBootServletInitializer {
